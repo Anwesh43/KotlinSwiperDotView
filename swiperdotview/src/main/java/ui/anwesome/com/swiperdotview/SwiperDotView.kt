9b83419b2f9d47b3f4319afed87ebea7946863eb
Anwesh43 <anweshthecool0@gmail.com>
2018-01-06 11:28:21 +0530

    adding DotMover

--- swiperdotview/src/main/java/ui/anwesome/com/swiperdotview/SwiperDotView.kt
+++ swiperdotview/src/main/java/ui/anwesome/com/swiperdotview/SwiperDotView.kt
@@ -19,4 +19,20 @@ class SwiperDotView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class DotMover(var x:Float,var y:Float,var size:Float = 0f,var w:Float = size) {
+        var updateFns:Array<(Float)->Unit> = arrayOf({scale -> w = size+size*scale},{scale ->
+            size = 2*size-size*scale
+            x = x+size*scale
+        })
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.parseColor("#E65100")
+            canvas.drawRoundRect(RectF(x-size/2,y-size/2,x+size/2,y+size/2),size/2,size/2,paint)
+        }
+        fun update(stopcb:()->Unit) {
+
+        }
+        fun startUpdating(statcb:()->Unit) {
+
+        }
+    }
 }
