7754db309d4c7216d296804319cf2149e937e229
Anwesh43 <anweshthecool0@gmail.com>
2018-01-06 13:06:14 +0530

    using swiperdotview in app

--- app/src/main/java/ui/anwesome/com/kotlinswiperdotview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinswiperdotview/MainActivity.kt
@@ -2,11 +2,13 @@ package ui.anwesome.com.kotlinswiperdotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.swiperdotview.SwiperDotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        SwiperDotView.create(this)
     }
 }
